af462209a1bdc3e5830257c605e41cdebc78e1ca
Simona Bisiani <simonabisiani1@gmail.com>
2021-02-23 13:39:17 +0100

    code and data for the local journalism project

---
+++ vita_flackar_18.R
@@ -0,0 +1,121 @@
+# libraries
+library(readxl)
+library(tidyverse)
+library(lubridate)
+library(fuzzyjoin)
+
+# to load all this already in your environment
+#load(file = "municipalities,funding,white spots.RData")
+
+#¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+#--- Which newsrooms existed in Sweden in 2018? 
+
+newsrooms <- read_csv("download.csv") # import kommundatabasen data from Mediestudier
+
+newsrooms <- newsrooms %>% 
+  mutate(sdate = ifelse(as.character(sdate) == "0", "1900-01-01", as.character(sdate)), #to avoid NAs when converting to date, I am putting a random date
+         sdate = ymd(sdate, truncated = 3), # convert to date (recognising both obs with only year, ym, or ymd format)
+         edate = ifelse(as.character(edate) == "9", "2021-01-01", as.character(edate)), #to avoid NAs when converting to date, I am putting a random date
+         edate = ymd(edate, truncated = 3))
+# (ignore error of 2 failed to parse, code works)
+
+newsrooms_2018 <- newsrooms %>% 
+  filter(sdate <= "2018-01-01", edate >= "2018-12-31")  # %>% subset for all newsrooms which existing during the entirety of 2018
+  #group_by(municipality_name) %>% 
+  #count() #how many did each municipality have?
+
+
+
+
+#¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+#--- Dataset of Swedish municipalities
+
+kommuner <- read_excel("kommuner_bra.xlsx", col_names = "municipality_name")
+
+kommuner <- kommuner %>% 
+  mutate(municipality_name = gsub("stad", "kommun", municipality_name)) %>%  # change "city" to "municipality" to merge with other dfs
+  mutate(name_short = str_replace(municipality_name, " kommun", "")) # for inexact matching purposes
+
+
+
+
+#¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+#--- Who received funding for local journalism? 
+
+#--- In 2019
+funding_19 <- read_excel("funding_2019.xlsx", col_names = c("newspaper", "amount", "name_short"))
+funding_19 <- funding_19 %>% 
+  filter(!is.na(newspaper) & !is.na(name_short)) # cleanup NAs
+funding_19$year <- "2019" # add year column      
+
+#--- In 2020
+funding_20 <- read_excel("funding_2020.xlsx", col_names = c("newspaper", "amount", "name_short"))
+funding_20 <- funding_20 %>% 
+  filter(!is.na(newspaper) & !is.na(name_short)) # cleanup NAs
+funding_20$year <- "2020" # add year column
+
+
+#--- Merge and cleanup
+funding <- bind_rows(list(funding_19, funding_20), .id = NULL)
+funding[8,3] <- "Älvkarleby" 
+funding[25,3] <- "Piteå, Älvsbyn, Luleå, Boden, Kalix, Överkalix, Arjeplog, Jokkmokk, Arvidsjaur"
+funding[88,3] <- "Krokom"
+funding <- funding[-167,]
+funding[80,3] <- "Älvkarleby"
+funding[150,3] <- "Tensta, Rinkeby"
+funding[200,3] <- "Sundsvall"
+funding[244,3] <- "Askersund, Laxå"
+funding[247,3] <- "Motala"
+funding <- funding %>% 
+  separate_rows(name_short, sep = ", ") #%>% # where one outlet received funding to cover one municipality, split to get one observation for each municipality (careful as money totals won't match anymore)
+  #group_by(name_short, year) %>%  #how many municipalities received funding in 2019 and 2020?
+  #count()
+
+
+#--- Which municipalities have received funding for two consecutive years?
+fund_19_20 <- funding[funding$name_short %in% names(which(table(funding$name_short) > 1)), ]
+
+funds_fuzzy <- fund_19_20 %>% 
+  stringdist_inner_join(kommuner, max_dist = 1) %>% 
+  select(-c(name_short.x, name_short.y)) %>% 
+  slice(-c(18, 104)) # Avesta and Alvesta are two different municipalities but matched using stringdist due to their close names. Here I remove the wrong arising matches
+
+  
+#--- This is a dataframe that only retains, within the funding, those observations that have a clear municipality targeted (removing for example "Västra Mälardalen")
+funding_clear_munic <- funding %>% 
+stringdist_inner_join(kommuner, by = c(name_short = "name_short"), max_dist = 1) %>% 
+slice(-c(19, 127)) %>% # again removing Avesta and Alvesta duplicates
+select(-c(name_short.x, name_short.y))
+
+
+
+
+
+#¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+#--- Dataset of white spots in 2018 (municipalities with no newsroom)
+
+white_spots_2018 <- anti_join(kommuner, newsrooms_2018, by = "municipality_name")
+
+#--- Which white spots did not request any funding?
+white_spots_18_no_funding <- anti_join(white_spots_2018, funds_fuzzy, by = "municipality_name") 
+
+
+
+
+#¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+
+#--- Dataframe to see all municipalities, outlets, amount and year of receiving funding
+
+alla_kommuner_funding <- left_join(kommuner, funding_clear_munic, by = "municipality_name") %>% 
+  select(-name_short) %>% 
+  group_by(municipality_name) %>% mutate(id=row_number()) %>%
+  pivot_longer(-c(municipality_name,id)) %>%
+  mutate(name=paste0(name,'.',id)) %>% select(-id) %>%
+  pivot_wider(names_from = name,values_from=value)
+
+
+#save.image(file = "municipalities,funding,white spots.RData")
